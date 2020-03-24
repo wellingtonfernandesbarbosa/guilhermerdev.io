4b5b433f49b817fc8f13c3955c7ba05d29bc63e2
Guilherme Lara <54341844+Guilherme-A-Lara@users.noreply.github.com>
2020-03-24 19:12:37 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -15,6 +15,22 @@ $(document).ready(function(e) {
     })
 
 });
+$(document).ready(function() {
+    // executes when HTML-Document is loaded and DOM is ready
+   console.log("document is ready");
+     
+   
+     $( ".card" ).hover(
+     function() {
+       $(this).addClass('shadow-lg').css('cursor', 'pointer'); 
+     }, function() {
+       $(this).removeClass('shadow-lg');
+     }
+   );
+     
+   // document ready  
+   });
+   
 
 function toggle_onclick($win, $navbar, width) {
     if ($win.width() <= 768) {
