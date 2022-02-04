205e3f7571c9edf35b87cafee56eca47b83cb87b
Guilherme Lara <guilherme.lara.sec@outlook.com>
2022-02-04 17:45:45 -0300

    port carousel

--- index.js
+++ index.js
@@ -70,3 +70,19 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+$('.carousel.carousel-multi-item.v-2 .carousel-item').each(function(){
+    var next = $(this).next();
+    if (!next.length) {
+      next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+  
+    for (var i=0;i<4;i++) {
+      next=next.next();
+      if (!next.length) {
+        next=$(this).siblings(':first');
+      }
+      next.children(':first-child').clone().appendTo($(this));
+    }
+  });
